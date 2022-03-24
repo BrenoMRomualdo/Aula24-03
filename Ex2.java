011bb0201426922b09fb53a35aedef825dccae7d
BrenoMR <aluno@IPIRANGA.FATEC>
2022-03-24 15:29:13 -0300

    Exercicio 2 feito

---
+++ Ex2.java
@@ -0,0 +1,13 @@
+public class Ex2 {
+        public static void main(String[] args) {
+         {
+        int numero_natural = 101;
+        int contador = 0;
+        do{numero_natural--;
+            System.out.printf("%d  ", numero_natural);
+            contador++;
+        }
+        while(contador <= 100);
+        }
+    }
+}
